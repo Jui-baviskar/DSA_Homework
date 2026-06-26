693dd3e42b01dc2019c6ca6bd4813afdf9c90d7b
Jui Baviskar <juibaviskar03@gmail.com>
2026-06-26 10:31:14 +0530

    program_13_5

--- program_13_5.java
+++ program_13_5.java
@@ -1,10 +1,36 @@
 import java.util.Scanner;
 
 public class program_13_5 {
-    public static void main(String A[])
-    {
-        Scanner sObj=new Scanner(System.in);
-        
-    }   
-    
+
+    public static void Display(int Arr[], int iLength) {
+        int iCnt = 0;
+
+        System.out.println("The elements which can be divisible by 5 ");
+
+        for (iCnt = 0; iCnt < iLength; iCnt++) {
+            if ((Arr[iCnt] % 11) == 0) {
+                System.out.println(Arr[iCnt]);
+            }
+        }
+    }
+
+    public static void main(String A[]) {
+        Scanner sObj = new Scanner(System.in);
+        int iSize = 0, iCnt = 0;
+        int[] Arr;
+
+        System.out.println("Enter number of elements ");
+
+        iSize = sObj.nextInt();
+        Arr = new int[iSize];
+
+        for (iCnt = 0; iCnt < iSize; iCnt++) {
+            System.out.print("Enter the element ");
+            Arr[iCnt] = sObj.nextInt();
+        }
+
+        Display(Arr, iSize);
+
+        sObj.close();
+    }
 }
