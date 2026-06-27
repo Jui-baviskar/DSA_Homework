13ef982d69b4a4f956ce579bf3be58db1d3ee2bf
Jui Baviskar <juibaviskar03@gmail.com>
2026-06-27 14:16:18 +0530

    program_20_2

---
+++ program_20_2.java
@@ -0,0 +1,17 @@
+class Logic{
+    void printReverse(int n)
+    {
+        int iCnt=0;
+        for(iCnt=n;iCnt>=1;iCnt--)
+        {
+            System.out.println(iCnt);
+        }
+    }
+}
+public class program_20_2 {
+    public static void main(String A[])
+    {
+        Logic lobj=new Logic();
+        lobj.printReverse(10);
+    }
+}
