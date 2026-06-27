7397c95bac99d620ee2f0ac193b7ecda61f54690
Jui Baviskar <juibaviskar03@gmail.com>
2026-06-27 14:13:35 +0530

    program_20_1

---
+++ program_20_1.java
@@ -0,0 +1,23 @@
+class Logic {
+    void sumEvenNumbers(int n)
+    {
+        int iCnt=0;
+        int iSum=0;
+        for(iCnt=1;iCnt<=n;iCnt++)
+        {
+            if(iCnt%2==0)
+            {
+                iSum+=iCnt;
+            }
+        }
+        System.out.println("The sum of even noumbers is "+iSum);
+    }
+    
+}
+public class program_20_1 {
+  public static void main(String A[])
+  {
+    Logic lobj=new Logic();
+    lobj.sumEvenNumbers(10);
+  }  
+}
