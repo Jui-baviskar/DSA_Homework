4a9ad205039c84ae2f41b5ee925b4bc58f8ec11e
Jui Baviskar <juibaviskar03@gmail.com>
2026-06-29 19:06:54 +0530

    program_21_4

---
+++ program_21_4.java
@@ -0,0 +1,19 @@
+class Logic{
+    void countFactors(int num) {
+        int iCnt = 0,iCount=0;
+        for (iCnt = 1; iCnt <= (num / 2); iCnt++) {
+            if (num % iCnt == 0) {
+                iCount++;
+            }
+        }
+        System.out.println("Count of Factors "+iCount);
+    }
+    
+}
+public class program_21_4 {
+    public static void main(String [] A)
+    {
+        Logic lObj=new Logic();
+        lObj.countFactors(20);
+    }
+}
