ebd09a8fa8ac49d10502309725afa1df961b4205
Jui Baviskar <juibaviskar03@gmail.com>
2026-07-06 21:50:24 +0530

    program_28_1

---
+++ program_28_1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0, k = 0;
+    for (i = iRow; i>0; i--)
+    {
+        for(j=1;j<=iCol;j++)
+        {
+            if(i==j)
+            {
+                printf("#\t");
+            }
+            else{
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1, iValue2;
+
+    printf("Enter number of rows and columns:\n");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
